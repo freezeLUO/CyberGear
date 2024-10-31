b7f3b5976cca0e4a165b2c7df95a5f7d67311762
VincentZhang <vencentzhang@outlook.com>
2024-10-31 21:43:26 +0800

    formart feedback

--- src/CyberGear.Control/CanBusBuilder.cs
+++ src/CyberGear.Control/CanBusBuilder.cs
@@ -1,41 +1,6 @@
-namespace CyberGear.Control
+namespace CyberGear.Control;
+
+public class CanBusBuilder
 {
-	/// <summary>
-	/// can 建造者
-	/// </summary>
-	public class CanBusBuilder
-	{
-		/// <summary>
-		/// can 配置
-		/// </summary>
-		public readonly CanbusOption CanbusOption;
-		/// <summary>
-		/// 插口类型
-		/// </summary>
-		public readonly SlotType SlotType;
-		/// <summary>
-		/// 插槽序号
-		/// </summary>
-		public readonly int SlotIndex;
-
-		/// <summary>
-		/// can 建造者
-		/// </summary>
-		/// <param name="slotType">插口类型</param>
-		/// <param name="slotIndex">插槽序号</param>
-		public CanBusBuilder(SlotType slotType, int slotIndex)
-		{
-			CanbusOption = new CanbusOption();
-			SlotType = slotType;
-			SlotIndex = slotIndex;
-		}
-
-		public void Configure(Action<CanbusOption> method)
-		{
-			method(CanbusOption);
-		}
-
-		public CanBus Build()
-			=> new CanBus(this);
-	}
+    
 }
